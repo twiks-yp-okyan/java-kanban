bd1c6fc8c80c4e182b5d97c908d67c818b2439e9
twiksokyan <twkscode@gmail.com>
2025-08-21 11:46:50 +0300

    fix#4 after github checkstyle test failure

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -4,9 +4,9 @@ import task.Task;
 import java.util.List;
 
 public interface HistoryManager {
-
     void add(Task task);
-    void remove(int id);
-    List<Task> getHistory();
 
+    void remove(int id);
+
+    List<Task> getHistory();
 }
